443f165e58f1dc56151c38fab12a2649960ad5df
Logan <alogantucker@gmail.com>
2023-04-10 15:40:41 -0400

    📦 NEW: Top ten tracks command complete

--- src/djs/commands/lastFMCommands/np.js
+++ src/djs/commands/lastFMCommands/np.js
@@ -70,12 +70,12 @@ module.exports = {
         const trackinfo = await axios.get(uri1) // the data
 
         //console.log(alltracks.data)
-        // get artist info from API
-let artistUri = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistname}&api_key=${lastFM_API_ID}&format=json`;
-const artistInfo = await axios.get(artistUri);
+        // get artist info from API - doesn't get a viable image
+// let artistUri = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistname}&api_key=${lastFM_API_ID}&format=json`;
+// const artistInfo = await axios.get(artistUri);
 
-// get the artist profile image url
-let artistImageURL = artistInfo.data.artist.image[3]["#text"];
+// // get the artist profile image url
+// let artistImageURL = artistInfo.data.artist.image[3]["#text"];
 
         const dom2 = new jsdom.JSDOM(trackinfo.data)
         let playcount;
@@ -91,7 +91,7 @@ let artistImageURL = artistInfo.data.artist.image[3]["#text"];
         if (albumimage == '') {
             albumimage = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F65441963%2Fhtml-file-upload-uploads-missing-icon-instead-of-chosen-image&psig=AOvVaw15V_is7ApKvri_t8awEQAf&ust=1681025684216000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDC3ujimf4CFQAAAAAdAAAAABAF';
         }
-        const embed = createEmb.createEmbed({color: userinfoget.hexAccentColor,author:{name: `LastFM User: ${LFuser.lastfmID}`, iconURL: `${avatar}`, url: `https://www.last.fm/user/${LFuser.lastfmID}`}, footer: { text: `Playcount: ${playcount} | Total Scrobbles: ${totalscrobbles} | Album: ${album}`, iconURL: artistname === "Juice WRLD" || "Juice Wrld" ? "https://lastfm.freetls.fastly.net/i/u/ar0/d6e904e50bb79e7877711efe9463c675.jpg" : artistImageURL}, fields: [
+        const embed = createEmb.createEmbed({color: userinfoget.hexAccentColor,author:{name: `LastFM User: ${LFuser.lastfmID}`, iconURL: `${avatar}`, url: `https://www.last.fm/user/${LFuser.lastfmID}`}, footer: { text: `Playcount: ${playcount} | Total Scrobbles: ${totalscrobbles} | Album: ${album}`, iconURL: artistname === "Juice WRLD" || "Juice Wrld" ? "https://lastfm.freetls.fastly.net/i/u/ar0/d6e904e50bb79e7877711efe9463c675.jpg" : ''}, fields: [
                 {name: 'Track', value: `> [${trackname}](${trackurl})`},
                 {name: 'Artist', value: `> [${artistname}](${artisturl})`}], thumbnail: albumimage})
            

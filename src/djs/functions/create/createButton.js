3b7f0f390b65c15bbbf0d201b2221c3db9632aab
Alogantucker <109883548+Alogantucker@users.noreply.github.com>
2023-05-11 14:28:24 -0400

    fix URL Issues w create Button

--- src/djs/functions/create/createButton.js
+++ src/djs/functions/create/createButton.js
@@ -3,24 +3,6 @@ const { ButtonBuilder, ButtonStyle } = require("discord.js");
 const scripts = require("../scripts/scripts.js");
 const scripts_mongoDB = require("../scripts/scripts_mongoDB.js");
 
-//   // Example Modal Object that gets passed in below
-// let buttonObj = {
-//     label: "🗜️ Compress File", { less than 45 characters }
-//     style: "link", { primary, secondary, success, danger, link}
-//     disabled: false, { true, false }
-//     emoji: "🗜️"
-//     link: "https://www.google.com",
-//
-// }
-// // OR
-// let buttonObj = {
-//     customID: "compress", { less than 100 characters }
-//     label: "🗜️ Compress File", { less than 45 characters }
-//     style: "primary", { primary, secondary, success, danger, link}
-//     disabled: true, { true, false }
-//     emoji: "🗜️"
-// }
-
 /**
  * Creates a new row using the provided button object.
  * @param {Object} buttonObj - The button object containing the button properties.
@@ -30,247 +12,95 @@ const scripts_mongoDB = require("../scripts/scripts_mongoDB.js");
  * @param {boolean} buttonObj.disabled - Indicates whether the button is disabled.
  * @param {string} buttonObj.emoji - The emoji of the button.
  * @param {string} buttonObj.link - The link of the button.
+ * @param {string} buttonObj.url - The url of the button (alternative to link property).
  */
 async function createButton(buttonObj, randID) {
   // destructure the buttonObj
-  let style = buttonObj.style ? buttonObj.style : "primary";
-  let disabled = buttonObj.disabled ? buttonObj.disabled : false;
-  let customID = buttonObj.customID
-  let label = buttonObj.label
-  let emoji = buttonObj.emoji
-  let link = buttonObj.link
+  const {
+    customID,
+    label,
+    style = "primary",
+    disabled = false,
+    emoji,
+    link,
+    url,
+  } = buttonObj;
 
   let button = new ButtonBuilder();
-  let lessCharsThan = (str, num) => {
-    let arr = [];
-    for (let i = 0; i < str.length; i++) {
-      arr.push(str.charAt(i));
-    }
-    if (arr.length <= num) {
-      return true;
-    } else {
-      return false;
-    }
-  };
 
-  // make sure each property is defined with isDefined(), which returns true if the property is defined
   if (!scripts.isDefined(customID) && style !== "link") {
-    try {
-      throw new Error("customID is not defined");
-    } catch (error) {
-      scripts.logError(error, "customID is not defined in createButton()");
-    }
+    scripts.logError(new Error("customID is not defined"), "customID is not defined in createButton()");
   } else {
-    // DO
-    // check to make sure it is a string that is within the character limit for a customID
-    // check to make sure custom id is less than or equal to 100 characters
-    // if its not throw an error and log with logError()
-    console.log(`the customID`, customID);
-
-    if (!scripts.isDefined(customID)) {
-      try {
-        if (randID) {
-          if (await scripts_mongoDB.getData(randID)) {
-            if (!link) {
-              try {
-                throw new Error("customID is not defined");
-              } catch (error) {
-                scripts.logError(error, "customID is not defined");
-              }
-            } else {
-
-              if (link !== null) {
-                button.setURL(link); 
-              }
-            }
-          } else {
-            button.setURL(`https://google.com/`);
-          }
-        } else {
-
-          // try {
-          //   throw new Error("customID is not defined");
-          // } catch (error) {
-          //   scripts.logError(error, "customID is not defined");
-          // }
-        }
-      } catch (error) {
-        scripts.logError(error, "err trying to get data in button create");
-      }
-    } else {
-      if (!lessCharsThan(customID, 100)) {
-        try {
-          throw new Error("customID is too long");
-        } catch (error) {
-          scripts.logError(error, "customID is too long: MAX 100 characters");
-          
-        }
-      }
-    }
-
-    if (style !== "link" || style !== "LINK" || style !== 5 || style !== "5") {
-      try {
+    if (style !== "link") {
+      if (scripts.isDefined(customID) && customID.length <= 100) {
         button.setCustomId(customID);
-      } catch (error) {
-        scripts.logError(error, "error w customID");
-      }
-    } else {
-
-      if (link !== null) {
-        button.setURL(link); 
-      }
-    }
-  }
-  if (!scripts.isDefined(label)) {
-    try {
-      throw new Error("label is not defined");
-    } catch (error) {
-      scripts.logError(error, "label is not defined");
-      // button.setLabel(" ");
-    }
-  } else {
-    if (!lessCharsThan(label, 80)) {
-      try {
-        throw new Error("label is too long: MAX 80 characters");
-      } catch (error) {
-        scripts.logError(error, "label is too long: MAX 80 characters");
-      }
-    }
-    button.setLabel(label);
-  }
-  if (scripts.isDefined(style)) {
-    // DO
-    // check to make sure it is a string that is one of the TextInputStyle options
-    if (
-      style !== "primary" &&
-      style !== "secondary" &&
-      style !== "success" &&
-      style !== "danger" &&
-      style !== "link" && style !== "PRIMARY" && style!== "SECONDARY" && style!== "SUCCESS" && style!== "DANGER" && style!== "LINK"
-      && style !== 1 &&
-      style !== 2 &&
-      style !== 3 &&
-      style !== 4 &&
-      style !== 5
-    ) {
-      try {
-        scripts.cLog(
-          "style is not a valid Button Style\nAuto Assigning primary"
-        );
-        style = "primary";
-      } catch (error) {
-        scripts.logError(error, "style is not a valid Button Style");
-      }
-    }
-    switch (style) {
-      case "primary":
-        button.setStyle(ButtonStyle.Primary);
-        break;
-      case "secondary":
-        button.setStyle(ButtonStyle.Secondary);
-        break;
-      case "success":
-        button.setStyle(ButtonStyle.Success);
-        break;
-      case "danger":
-        button.setStyle(ButtonStyle.Danger);
-        break;
-      case "link":
-        button.setStyle(ButtonStyle.Link);
-        break;
-        case "PRIMARY":
-        button.setStyle(ButtonStyle.Primary);
-        break;
-        case "SECONDARY":
-        button.setStyle(ButtonStyle.Secondary);
-        break;
-        case "SUCCESS":
-        button.setStyle(ButtonStyle.Success);
-        break;
-        case "DANGER":
-        button.setStyle(ButtonStyle.Danger);
-        break;
-        case "LINK":
-        button.setStyle(ButtonStyle.Link);
-        break;
-        case "1":
-        button.setStyle(ButtonStyle.Primary);
-        break;
-      case "2":
-        button.setStyle(ButtonStyle.Secondary);
-        break;
-      case "3":
-        button.setStyle(ButtonStyle.Success);
-        break;
-      case "4":
-        button.setStyle(ButtonStyle.Danger);
-        break;
-      case "5":
-        button.setStyle(ButtonStyle.Link);
-        break;
-        case 1:
-        button.setStyle(ButtonStyle.Primary);
-        break;
-      case 2:
-        button.setStyle(ButtonStyle.Secondary);
-        break;
-      case 3:
-        button.setStyle(ButtonStyle.Success);
-        break;
-      case 4:
-        button.setStyle(ButtonStyle.Danger);
-        break;
-      case 5:
-        button.setStyle(ButtonStyle.Link);
-        break;
-      default:
-        button.setStyle(ButtonStyle.Primary);
-        break;
-    }
-    if (button.style === "link") {
-      if (!scripts.isDefined(link)) {
-        try {
-          throw new Error("link is not defined");
-        } catch (error) {
-          scripts.logError(error, "link is not defined");
-        }
       } else {
-        if (link !== null) {
-          button.setURL(link); 
-        }
+        scripts.logError(new Error("customID is too long"), "customID is too long: MAX 100 characters");
       }
+    } else if (link) {
+      button.setURL(link);
+    } else if (url) {
+      button.setURL(url);
+    }
+  }
+
+  if (scripts.isDefined(label)) {
+    if (label.length <= 80) {
+      button.setLabel(label);
+    } else {
+      scripts.logError(new Error("label is too long"), "label is too long: MAX 80 characters");
+    }
+  }
+
+  const styleMap = {
+    primary: ButtonStyle.Primary,
+    secondary: ButtonStyle.Secondary,
+    success: ButtonStyle.Success,
+    danger: ButtonStyle.Danger,
+    link: ButtonStyle.Link,
+    "1": ButtonStyle.Primary,
+    "2": ButtonStyle.Secondary,
+    "3": ButtonStyle.Success,
+    "4": ButtonStyle.Danger,
+    "5": ButtonStyle.Link,
+  };
+  
+  if (scripts.isDefined(style)) {
+    let buttonStyle = styleMap[style.toString().toLowerCase()];
+  
+    if (buttonStyle) {
+      button.setStyle(buttonStyle);
+    } else {
+      scripts.logError(new Error("style is not valid"), "style is not valid");
+      button.setStyle(ButtonStyle.Primary);
     }
   } else {
     button.setStyle(ButtonStyle.Primary);
   }
+  
+
+  if (button?.data?.style === ButtonStyle.Link && (scripts.isDefined(link) || scripts.isDefined(url))) {
+    button.setURL(link || url);
+  }
 
   if (scripts.isDefined(disabled)) {
-    if (typeof disabled !== "boolean") {
-      try {
-        scripts.cLog(
-          "disabled is not a boolean\nAuto Assigning disabled to false"
-        );
-        disabled = false;
-      } catch (error) {
-        scripts.logError(error, "disabled is not a boolean");
-      }
+    if (typeof disabled === "boolean") {
+      button.setDisabled(disabled);
+    } else {
+      scripts.logError(new Error("disabled is not a boolean"), "disabled is not a boolean");
+      button.setDisabled(false);
     }
-    button.setDisabled(disabled);
   } else {
     button.setDisabled(false);
   }
+
   if (scripts.isDefined(emoji)) {
-    if (typeof emoji !== "string") {
-      try {
-        scripts.cLog("emoji is not a string\nAuto Assigning emoji to null");
-        emoji = null;
-      } catch (error) {
-        scripts.logError(error, "emoji is not a string");
-      }
+    if (typeof emoji === "string") {
+      button.setEmoji(emoji);
+    } else {
+      scripts.logError(new Error("emoji is not a string"), "emoji is not a string");
     }
-    button.setEmoji(emoji);
   }
-  console.log(`the button`, button);
 
   return button;
 }
